6199825d345cee610c812956c88ec6f79a8227b7
kimwoojin <kimwoojin1903@gmail.com>
2023-10-11 10:42:25 +0900

    this 함수 확인

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function a() {
-    console.log(this);
+function a(callback) {
+    return callback();
 }
+
+console.log(
+    a(function () {
+        console.log(this);
+        console.log('--------------------');
+        console.log(global);
+    })
+);
