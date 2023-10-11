90b34479a1b5f19da278fd09e5cd3f65849f457b
kimwoojin <kimwoojin1903@gmail.com>
2023-10-11 10:57:23 +0900

    this 함수를 이용하여 배열로 객체 만들기

--- newfile.js
+++ newfile.js
@@ -1,7 +1,13 @@
-function student(name, number) {
+function Student(name, number) {
     this.name = name;
     this.number = number;
 }
 
-let test = new student('공욱재', '0');
-console.log(test);
+let list = ['윤준현', '이민구'];
+let studentArr = [];
+
+for (let i = 0; i < list.length; i++) {
+    studentArr[i] = new Student(list[i], i + 1);
+}
+
+console.log(studentArr);
